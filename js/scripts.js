eeffb1abb8506e913a0a29edd1dea4238664d772
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-09 16:04:54 -0700

    Simplify if else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    $("this").remove();
     var nameInput = ($("input#name").val()).toUpperCase();
     var work = parseInt($("input:radio[name=work]:checked").val());
     var frontEnd = $("input:radio[name=front-end]:checked").val();
@@ -9,10 +8,10 @@ $(document).ready(function() {
     var startDate = parseInt($("input:radio[name=start-date]:checked").val());
 
     if (work + loveAndroid + build + startDate === 4) {
-      $("#ruby-rails").fadeIn("slow").delay(9000).hide(1);
-    } else if (frontEnd === "false" && work + loveAndroid + build + startDate > 4) {
       $("#java-android").fadeIn("slow").delay(9000).hide(1);
-    } else if (frontEnd !== "false" && work + loveAndroid + build + startDate > 4) {
+    } else if (frontEnd === "false" && work + loveAndroid + build + startDate > 4) {
+      $("#ruby-rails").fadeIn("slow").delay(9000).hide(1);
+    } else {
       $("#css-design").fadeIn("slow").delay(9000).hide(1);
     }
 
