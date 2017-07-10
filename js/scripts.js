d64d1beac438e57edfee67a15c70b85eb609d287
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-09 17:14:57 -0700

    Further simplify else if statement.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
     var nameInput = ($("input#name").val()).toUpperCase();
     var work = parseInt($("input:radio[name=work]:checked").val());
     var frontEnd = $("input:radio[name=front-end]:checked").val();
-    var loveAndroid = parseInt($("input:radio[name=love-android]:checked").val());
+    var loveAndroid = $("input:radio[name=love-android]:checked").val();
     var build = parseInt($("input:radio[name=build]:checked").val());
     var startDate = parseInt($("input:radio[name=start-date]:checked").val());
 
-    if (work + loveAndroid + build + startDate === 4) {
+    if (loveAndroid === "true" && frontEnd === "false") {
       $("#java-android").fadeIn("slow").delay(9000).hide(1);
-    } else if (frontEnd === "false" && work + loveAndroid + build + startDate > 4) {
-      $("#ruby-rails").fadeIn("slow").delay(9000).hide(1);
-    } else {
+    } else if ((frontEnd === "true" && loveAndroid === "false") || (frontEnd === "true" && loveAndroid === "true")) {
       $("#css-design").fadeIn("slow").delay(9000).hide(1);
+    } else {
+      $("#ruby-rails").fadeIn("slow").delay(9000).hide(1);
     }
 
     $(".name").text(nameInput);
